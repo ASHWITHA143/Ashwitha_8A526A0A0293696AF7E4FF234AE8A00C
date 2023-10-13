618a4ab321425024562952a8717eb0ead3dd54d0
ASHWITHA143 <144425377+ASHWITHA143@users.noreply.github.com>
2023-10-13 12:26:00 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,10 +1,24 @@
-def isLeapYear(year):
-  if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
-    return True
-  else:
-    return False
-year = int(input("Enter a year : "))
-if isLeapYear(year):
-  print('{} is a leap year.'.format(year))
-else:
-  print('{} is not a leap year).'.format(year))
+def sort_students(student_list):
+    sorted_students = sorted(student_list, key=lambda x: x.cgpa, reverse=True)
+    return sorted_students
+
+# Define the Student class
+class Student:
+    def __init__(self, name, roll_number, cgpa):
+        self.name = name
+        self.roll_number = roll_number
+        self.cgpa = cgpa
+
+# Test the function with a list of student objects
+students = [
+    Student("Alice", "A101", 3.9),
+    Student("Bob", "B102", 3.7),
+    Student("Charlie", "C103", 3.5),
+    Student("David", "D104", 3.8),
+]
+
+sorted_students = sort_students(students)
+
+# Print the sorted list of students
+for student in sorted_students:
+    print(f"Name: {student.name}, Roll Number: {student.roll_number}, CGPA: {student.cgpa}")
--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-def fact_rec(n):
-  if n==0 or n==1:
-    return 1
-  else:
-    return n*fact_rec(n-1)
-number=int(input("Enter a value :"))
-res = fact_rec (number)
-print("The factorial of {} is {}.". format (number,res))
+def linear_search_product(products, target_product):
+    indices = []
+    for i in range(len(products)):
+        if products[i] == target_product:
+            indices.append(i)
+    return indices
+# Create a list of products
+products = ["Apple", "Banana", "Orange", "Apple", "Grapes", "Apple"]
+
+# Search for the target product "Apple"
+target_product = "Apple"
+result = linear_search_product(products, target_product)
+
+# Print the result
+if len(result) > 0:
+    print(f"The product '{target_product}' was found at indices: {result}")
+else:
+    print(f"The product '{target_product}' was not found.")
